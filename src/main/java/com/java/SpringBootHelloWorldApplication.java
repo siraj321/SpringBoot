62286c123288b0ba58290f2f2acb57837b2f353a
siraj321 <siraj.diwan@rediffmail.com>
2020-01-21 17:31:20 +0530

    url added

--- src/main/java/com/java/SpringBootHelloWorldApplication.java
+++ src/main/java/com/java/SpringBootHelloWorldApplication.java
@@ -4,6 +4,9 @@ package com.java;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ https://dzone.com/articles/spring-boot-security-json-web-tokenjwt-hello-world
+ */
 @SpringBootApplication
 public class SpringBootHelloWorldApplication {
 	public static void main(String[] args) {
